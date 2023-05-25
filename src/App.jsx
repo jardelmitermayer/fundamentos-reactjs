f17270f7106f00e96186f5df060945fe2aaf0ae4
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2023-05-25 02:01:21 -0300

    post component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Jardel" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptates nostrum quaerat blanditiis ea optio tenetur aperiam expedita. Vero corporis nulla officia eos quae. Esse nulla ullam nesciunt repudiandae itaque."  
+      />
+      <Post 
+        author="Autor 2" 
+        content="Outro post"  
+      />
     </div>
   )
 }
+
