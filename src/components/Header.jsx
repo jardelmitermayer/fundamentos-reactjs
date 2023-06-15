664a07be774cc4b3ce682ca36c1da4bd82b10dcb
Jardel Mitermayer <jardelska@gmail.com>
2023-06-15 01:52:14 -0300

    style: add image to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.png'
+
 export function Header() {
- return (
-  <strong className={styles.header}>Ignite Feed</strong>
- )
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo ignite" />
+    </header>
+  )
 
 }
