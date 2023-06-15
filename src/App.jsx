2bc4f944ea101e1ad34bf252d0285e46c2eccfa5
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2023-06-15 00:08:40 -0300

    style: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 
 import './global.css'
@@ -5,13 +6,14 @@ import './global.css'
 export function App() {
   return (
     <div>
-      <Post 
-        author="Jardel" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptates nostrum quaerat blanditiis ea optio tenetur aperiam expedita. Vero corporis nulla officia eos quae. Esse nulla ullam nesciunt repudiandae itaque."  
+      <Header />
+      <Post
+        author="Jardel"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptates nostrum quaerat blanditiis ea optio tenetur aperiam expedita. Vero corporis nulla officia eos quae. Esse nulla ullam nesciunt repudiandae itaque."
       />
-      <Post 
-        author="Autor 2" 
-        content="Outro post"  
+      <Post
+        author="Autor 2"
+        content="Outro post"
       />
     </div>
   )
