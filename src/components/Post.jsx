bcd27681cb3f350fb96ff11e86d965af94afb095
Mitermayer18 <jardelska@gmail.com>
2023-10-11 00:57:21 -0300

    feat: create Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,35 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/58043717?v=4' />
+          <div className={styles.authorInfo}>
+            <strong>Jardel Mitermayer</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title='05 de outubro às 06:13' dateTime='2023-10-05 06:13:40'>Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
+
+        <p><a href="">jane.design/doctorcare </a></p>
+
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a>#nlw </a>{' '}
+          <a>#rocketseat</a>
+        </p>
+
+
+      </div>
+    </article>
+  )
+}
