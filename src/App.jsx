ce9dc230bbcef4df2ab41a08477d162ab04cae3b
Mitermayer18 <jardelska@gmail.com>
2023-10-15 23:39:30 -0300

    feat: create Post

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Jardel"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptates nostrum quaerat blanditiis ea optio tenetur aperiam expedita. Vero corporis nulla officia eos quae. Esse nulla ullam nesciunt repudiandae itaque."
-          />
-          <Post
-            author="Autor 2"
-            content="Outro post"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
 
